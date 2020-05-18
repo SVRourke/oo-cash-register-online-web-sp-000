40dae83960af2c459a1156850a993e8df947850c
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-18 04:53:10 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   end
 
   def apply_discount()
-    percentage = @discount / 100
+    percentage = @discount.to_f / 100
     puts percentage
     discount_value = @total * percentage
     puts discount_value
