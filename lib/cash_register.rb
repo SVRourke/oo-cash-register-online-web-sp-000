6eeb50ce804c0207835d965205120f89bb8a318b
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-18 05:19:19 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@ class CashRegister
   attr_accessor :discount, :total, :items, :previous
 
   def initialize(discount =0)
-    @total = 100
+    @total = 0
     @discount = discount
     @items = Array.new
   end
