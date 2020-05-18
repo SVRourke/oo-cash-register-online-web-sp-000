21a89c9b7e4387dccd878fe5d147dbc7114b3abb
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-18 04:41:04 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 class CashRegister
-  def initialize(employee_discount=0)
+  def initialize(discount =0)
     @total = 0
-    @employee_discount = employee_discount
+    @discount  = discount
   end
 
 end
